666983a570020e8d961315028ca6ed23e59945c5
gabbayron <gabbayron2307@gmail.com>
2020-05-21 00:16:13 +0300

    shorte version

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 // ------Variables ----
 
-// var red = document.getElementById('red')
-// var green = document.getElementById('green')
-// var blue = document.getElementById('blue')
-// var opacity = document.getElementById('opa')
-
 var colorPick = document.getElementById('colorPick')
 var divPickedColor = document.getElementById('pickedColor')
 
 // -------Def rgba Object -----
+
 let rgba = {
     red: 0,
     green: 0,
@@ -19,8 +15,6 @@ colorPick.addEventListener('input', setColor);
 
 function setColor(e) {
     rgba[e.target.id] = e.target.valueAsNumber
-    // rgba[colorId] = e.target.valueAsNumber
     let rgbaColor = `rgba(${rgba.red} , ${rgba.green} , ${rgba.blue} , ${rgba.opacity})`
     divPickedColor.style.background = rgbaColor
-    // document.body.style.color = 'blue'
 }
